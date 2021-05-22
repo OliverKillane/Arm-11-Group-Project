c26636f015e804fd860eeb49a2ef8eadc1b8fe24
barci2 <bcieslar2001@gmail.com>
2021-05-22 11:03:05 +0100

    Implementation and tests for ProcessDataProcessing

--- src/commandgen.c
+++ src/commandgen.c
@@ -3,28 +3,38 @@
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
+#include <stdio.h>
 
 #define FUNC_PROC_ARGS Map restrict symbols, Vector restrict tokens,\
                        Vector restrict output, int offset, int instructions_num
+static Map restrict dummy;
 static Map restrict func_proc; // Map<char*, void(*func)(FUNC_PROC_ARGS)
 static Map restrict func_cond_codes; // Map<char*, char>
+static Map restrict data_proc_opcodes; // Map<char*, char>
+static Set restrict comp_funcs; // Set<char*>
+static Map restrict shift_codes;
+const unsigned int MAX_FUNCTION_LENGTH = 3;
 
 /* Helper Functions */
-char GetCondCode(char* func_code) {
-    if(strlen(func_code) < 2) {
-        return MapGet(func_cond_codes, "al");
+void SplitFunction(
+    char* restrict func_code, 
+    char* restrict func_base, 
+    unsigned int* restrict condition
+) {
+    if(MapQuery(func_proc, func_code)) {
+        strcpy(func_base, func_code);
+        *condition = MapGet(func_cond_codes, "al");
+        return;
     }
     char* suffix = func_code + strlen(func_code) - 2;
-    if(!MapQuery(func_cond_codes, suffix)) {
-        return MapGet(func_cond_codes, "al");
-    } else {
-        return MapGet(func_cond_codes, suffix);
-    }
+    assert(MapQuery(func_cond_codes, suffix));
+    strncpy(func_base, func_code, strlen(func_code) - 2);
+    *condition = MapGet(func_cond_codes, suffix);
 }
 
 unsigned int GetExpressionValue(
-    Map symbols, 
-    char* expression, 
+    Map restrict symbols, 
+    char* restrict expression, 
     bool pureexpression, 
     bool hashexpression
 ) {
@@ -63,7 +73,7 @@ unsigned int GetExpressionValue(
                 base = 8;
         }
     }
-    unsigned int out = 0;
+    unsigned long long out = 0;
     for(start; start < expr_len; start++) {
         char c = expression[start];
         int value;
@@ -76,20 +86,48 @@ unsigned int GetExpressionValue(
         }
         assert(value >= 0 && value < base);
         out = out * base + value;
+        assert(out < ((long long)1<<32));
     }
     return out;
 }
 
-void SetInstruction(Vector output, int instruction, int offset) {
+void SetInstruction(Vector restrict output, int instruction, int offset) {
     if(VectorSize(output) <= offset) {
         VectorResize(output, offset + 1);
     }
     VectorSet(output, offset, instruction);
 }
 
+int GetRegisterNum(char* restrict reg) {
+    assert(reg[0] == 'r' || reg[0] == 'R');
+    assert(strlen(reg)>=2);
+    int reg_num = atoi(reg + 1);
+    assert(reg_num >= 0 && reg_num <= 16);
+    return reg_num;
+}
+
+bool IsRegister(char* restrict token) { // may also be a label
+    if(token[0] != 'r' && token[0] != 'R') {
+        return false;
+    }
+    if(strlen(token) < 2) {
+        return false;
+    }
+    for(int i = 1; i < strlen(token); i++) {
+        if(!isdigit(token[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 /* Function Processing Functions */
 void ProcessBranch(FUNC_PROC_ARGS) {
     assert(VectorSize(tokens) == 2);
+
+    unsigned int condition;
+    char dummy[MAX_FUNCTION_LENGTH + 1];
+    SplitFunction(VectorGet(tokens, 0), dummy, &condition);
 
     const int target = GetExpressionValue(symbols, VectorGet(tokens, 1), true, true);
     assert(((target >> 2) << 2) == target);
@@ -99,14 +137,104 @@ void ProcessBranch(FUNC_PROC_ARGS) {
     assert((2<<23) <= pc_offset < (2 << 23));
 
     unsigned int instr = 0x0A000000;
-    instr |= (unsigned int)GetCondCode(VectorGet(tokens, 0)) << 28;
+    instr |= condition << 28;
     instr |= pc_offset & 0xFFFFFF;
 
     SetInstruction(output, instr, offset);
 }
 
 void ProcessDataProcessing(FUNC_PROC_ARGS) {
+    const int tokens_num = VectorSize(tokens);
+    assert(tokens_num >= 2);
+    
+    unsigned int condition;
+    char func_base[MAX_FUNCTION_LENGTH + 1];
+    SplitFunction(VectorGet(tokens, 0), func_base, &condition);
+    assert(MapQuery(data_proc_opcodes, func_base));
+    int opcode = MapGet(data_proc_opcodes, func_base);
 
+    assert(IsRegister(VectorGet(tokens, 1)));
+    unsigned int reg_d, reg_n, operand_start, set_cspr;
+    if(StringEq(func_base, "mov")) {
+        reg_d = GetRegisterNum(VectorGet(tokens, 1));
+        reg_n = 0;
+        operand_start = 2;
+        set_cspr = 0;
+    } else if(SetQuery(comp_funcs, func_base)) {
+        reg_d = 0;
+        reg_n = GetRegisterNum(VectorGet(tokens, 1));
+        operand_start = 2;
+        set_cspr = 1;
+    } else {
+        assert(tokens_num >= 3);
+        assert(IsRegister(VectorGet(tokens, 2)));
+        reg_d = GetRegisterNum(VectorGet(tokens, 1));
+        reg_n = GetRegisterNum(VectorGet(tokens, 2));
+        operand_start = 3;
+        set_cspr = 0;
+    }
+    assert(reg_d <= 12 && reg_n <= 12);
+
+    assert(tokens_num > operand_start);
+    unsigned int immediate = ((char*)VectorGet(tokens, operand_start))[0] == '#';
+
+    unsigned int operand2 = 0;
+    if(immediate) {
+        assert(tokens_num == operand_start + 1);
+        unsigned int expr_value = GetExpressionValue(
+            dummy, VectorGet(tokens, operand_start), false, true
+        );
+        unsigned int rotate_left = 0;
+        while((expr_value & 0b11) == 0 && expr_value > 0) {
+            rotate_left++;
+            expr_value >>= 2;
+        }
+        assert(expr_value < (1<<8));
+        operand2 |= (rotate_left == 0 ? 0 : 16 - rotate_left) << 8;
+        operand2 |= expr_value;
+    } else {
+        assert(IsRegister(VectorGet(tokens, operand_start)));
+        unsigned int reg_m = GetRegisterNum(VectorGet(tokens, operand_start));
+        assert(reg_m <= 12);
+        
+        unsigned int shift = 0;
+        if(tokens_num > operand_start + 1) {
+            assert(tokens_num == operand_start + 3);
+            assert(MapQuery(shift_codes, VectorGet(tokens, operand_start + 1)));
+            unsigned int shift_code = MapGet(shift_codes, VectorGet(tokens, operand_start + 1));
+
+            if(((char*)VectorGet(tokens, operand_start + 2))[0] == '#') {
+                unsigned int shamt = GetExpressionValue(
+                    dummy, VectorGet(tokens, operand_start + 2), false, true
+                );
+                shift = 0;
+                shift |= shift_code << 1;
+                shift |= shamt << 3;
+
+            } else {
+                assert(IsRegister(VectorGet(tokens, operand_start + 2)));
+                unsigned int reg_s = GetRegisterNum(VectorGet(tokens, operand_start + 2));
+                assert(reg_s <= 12);
+
+                shift = 1;
+                shift |= shift_code << 1;
+                shift |= reg_s << 4;
+            }
+        }
+
+        operand2 |= reg_m;
+        operand2 |= shift << 4;
+    }
+
+    unsigned int instr = 0;
+    instr |= condition << 28;
+    instr |= immediate << 25;
+    instr |= opcode << 21;
+    instr |= set_cspr << 20;
+    instr |= reg_n << 16;
+    instr |= reg_d << 12;
+    instr |= operand2;
+    SetInstruction(output, instr, offset);
 }
 
 void ProcessMultiply(FUNC_PROC_ARGS) {
@@ -117,12 +245,16 @@ void ProcessDataTransfer(FUNC_PROC_ARGS) {
     
 }
 
+void ProcessShift(FUNC_PROC_ARGS) {
+
+}
+
 void InitFuncProc() {
     func_proc = NewEmptyMap(StringHash, StringEq);
     MapSet(func_proc, "b", ProcessBranch);
-    int data_proc_func_count = 11;
+    int data_proc_func_count = 10;
     char* data_proc_funcs[] = {
-        "add", "sub", "rsb", "and", "eor", "orr", "mov", "tst", "teq", "cmp", "lsl"
+        "add", "sub", "rsb", "and", "eor", "orr", "mov", "tst", "teq", "cmp"
     };
     for(int i = 0; i < data_proc_func_count; i++) {
         MapSet(func_proc, data_proc_funcs[i], ProcessDataProcessing);
@@ -131,6 +263,10 @@ void InitFuncProc() {
     MapSet(func_proc, "mla", ProcessMultiply);
     MapSet(func_proc, "ldr", ProcessDataTransfer);
     MapSet(func_proc, "str", ProcessDataTransfer);
+    MapSet(func_proc, "lsl", ProcessShift);
+    MapSet(func_proc, "asr", ProcessShift);
+    MapSet(func_proc, "lsr", ProcessShift);
+    MapSet(func_proc, "ror", ProcessShift);
 }
 
 void InitFuncCondCodes() {
@@ -144,29 +280,60 @@ void InitFuncCondCodes() {
     MapSet(func_cond_codes, "al", 0b1110);
 }
 
+void InitDataProcOpcodes() {
+    data_proc_opcodes = NewEmptyMap(StringHash, StringEq);
+    MapSet(data_proc_opcodes, "and", 0b0000);
+    MapSet(data_proc_opcodes, "eor", 0b0001);
+    MapSet(data_proc_opcodes, "sub", 0b0010);
+    MapSet(data_proc_opcodes, "rsb", 0b0011);
+    MapSet(data_proc_opcodes, "add", 0b0100);
+    MapSet(data_proc_opcodes, "orr", 0b1100);
+    MapSet(data_proc_opcodes, "mov", 0b1101);
+    MapSet(data_proc_opcodes, "tst", 0b1000);
+    MapSet(data_proc_opcodes, "teq", 0b1001);
+    MapSet(data_proc_opcodes, "cmp", 0b1010);
+}
+
+void InitCompFuncs() {
+    comp_funcs = NewEmptySet(StringHash, StringEq);
+    SetInsert(comp_funcs, "tst");
+    SetInsert(comp_funcs, "teq");
+    SetInsert(comp_funcs, "cmp");
+}
+
+void InitShiftCodes() {
+    shift_codes = NewEmptyMap(StringHash, StringEq);
+    MapSet(shift_codes, "lsl", 0b00);
+    MapSet(shift_codes, "lsr", 0b01);
+    MapSet(shift_codes, "asr", 0b10);
+    MapSet(shift_codes, "ror", 0b11);
+}
+
 /* Interface Definitions */
 void InitFunctionGen() {
     InitFuncProc();
     InitFuncCondCodes();
+    InitDataProcOpcodes();
+    InitCompFuncs();
+    InitShiftCodes();
+    dummy = NewEmptyMap(StringHash, StringEq);
 }
 
 void FunctionGen(FUNC_PROC_ARGS) {
     char* func_code = VectorGet(tokens, 0);
-    int func_code_len = strlen(func_code);
-    char* raw_func_code = malloc(sizeof(char) * (func_code_len + 1));
-    if(func_code_len <= 1) {
-        strcpy(raw_func_code, func_code);
-    } else if(MapQuery(func_cond_codes, func_code + func_code_len - 2)) {
-        strncpy(raw_func_code, func_code, func_code_len - 2);
-    } else {
-        strcpy(raw_func_code, func_code);
-    }
-    void(*process_function)(FUNC_PROC_ARGS) = MapGet(func_proc, raw_func_code);
+    unsigned int condition;
+    char func_base[MAX_FUNCTION_LENGTH + 1];
+    SplitFunction(func_code, func_base, &condition);
+
+    void(*process_function)(FUNC_PROC_ARGS) = MapGet(func_proc, func_base);
     assert(process_function != NULL);
     process_function(symbols, tokens, output, offset, instructions_num);
 }
 
 void FinishFunctionGen() {
+    DeleteMap(dummy);
     DeleteMap(func_proc);
     DeleteMap(func_cond_codes);
+    DeleteMap(data_proc_opcodes);
+    DeleteSet(comp_funcs);
 }
--- src/commandgentests.c
+++ src/commandgentests.c
@@ -1,19 +1,5 @@
 #include "commandgen.c"
 
-/* GetCondCode tests */
-void GetCondCodeTests() {
-    InitFunctionGen();
-    assert(GetCondCode("andeq") == 0b0000);
-    assert(GetCondCode("orne") == 0b0001);
-    assert(GetCondCode("movge") == 0b1010);
-    assert(GetCondCode("blt") == 0b1011);
-    assert(GetCondCode("eorgt") == 0b1100);
-    assert(GetCondCode("mulle") == 0b1101);
-    assert(GetCondCode("ldral") == 0b1110);
-    assert(GetCondCode("cmp") == 0b1110);
-    FinishFunctionGen();
-}
-
 /* GetExpressionValue tests */
 void GetExprValueTests() {
     Map symbols = NewEmptyMap(StringHash, StringEq);
@@ -69,16 +55,62 @@ void ProcessBranchTests() {
     assert((int)VectorGet(output, 6) == 0xEAFFFFFA);
     assert((int)VectorGet(output, 7) == 0xEAFFFFFE);
     FinishFunctionGen();
+    DeleteMap(symbols);
+    DeleteVector(output);
 }
 
 /* ProcessDataProcessing tests */
+void SingleProcessDataProcessing(
+    Map symbols, 
+    int tokens_num, 
+    char* tokens[], 
+    Vector output, 
+    int offset, 
+    int instruction_num
+) {
+    Vector tokens_vector = NewVector(tokens_num);
+    for(int i = 0; i < tokens_num; i++) {
+        VectorSet(tokens_vector, i, tokens[i]);
+    }
+    ProcessDataProcessing(symbols, tokens_vector, output, offset, instruction_num);
+    DeleteVector(tokens_vector);
+}
+void ProcessDataProcessingTests() {
+    Map symbols = NewEmptyMap(StringHash, StringEq);
+    InitFunctionGen();
+    Vector output = NewEmptyVector();
+
+    SingleProcessDataProcessing(symbols, 4, (char*[]){"andeq", "R12", "R12", "R9"}, output, 0, 10);
+    SingleProcessDataProcessing(symbols, 4, (char*[]){"eorne", "R8", "R6", "#0xFF00"}, output, 1, 10);
+    SingleProcessDataProcessing(symbols, 4, (char*[]){"subge", "R4", "R4", "#0b10100"}, output, 2, 10);
+    SingleProcessDataProcessing(symbols, 4, (char*[]){"rsblt", "R1", "R9", "#0x3FC0"}, output, 3, 10);
+    SingleProcessDataProcessing(symbols, 6, (char*[]){"addgt", "R3", "R2", "R2", "asr", "R9"}, output, 4, 10);
+    SingleProcessDataProcessing(symbols, 6, (char*[]){"orrle", "R5", "R1", "R3", "lsl", "R0"}, output, 5, 10);
+    SingleProcessDataProcessing(symbols, 5, (char*[]){"moval", "R7", "R6", "lsr", "#0o30"}, output, 6, 10);
+    SingleProcessDataProcessing(symbols, 5, (char*[]){"tst", "R9", "R9", "ror", "#0d11"}, output, 7, 10);
+    SingleProcessDataProcessing(symbols, 5, (char*[]){"teq", "R11", "R0", "lsr", "#0xF"}, output, 8, 10);
+    SingleProcessDataProcessing(symbols, 3, (char*[]){"cmp", "R10", "#261120"}, output, 9, 10);
+    
+    assert((int)VectorGet(output, 0) == 0x000CC009);
+    assert((int)VectorGet(output, 1) == 0x12268CFF);
+    assert((int)VectorGet(output, 2) == 0xA2444F05);
+    assert((int)VectorGet(output, 3) == 0xB2691DFF);
+    assert((int)VectorGet(output, 4) == 0xC0823952);
+    assert((int)VectorGet(output, 5) == 0xD1815013);
+    assert((int)VectorGet(output, 6) == 0xE1A07C26);
+    assert((int)VectorGet(output, 7) == 0xE11905E9);
+    assert((int)VectorGet(output, 8) == 0xE13B07A0);
+    assert((int)VectorGet(output, 9) == 0xE35A0BFF);
+}
 
 /* ProcessMultiply tests */
 
 /* ProcessDataTransfer tests */
 
+/* ProcessShift tests */
+
 int main() {
-    GetCondCodeTests();
     GetExprValueTests();
     ProcessBranchTests();
+    ProcessDataProcessingTests();
 }
