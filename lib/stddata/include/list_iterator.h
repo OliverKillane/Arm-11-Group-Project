18b4688efa7c71955feaab90a0da8ddeaa60852a
barci2 <bcieslar2001@gmail.com>
2021-05-23 23:15:57 +0100

    Synchronized the stddata library with master

--- lib/stddata/include/list_iterator.h
+++ lib/stddata/include/list_iterator.h
@@ -1,69 +1,145 @@
+/**
+ * @file list_iterator.h 
+ * @author Bartłomiej Cieślar
+ * @brief @ref ListIterator and utilities related to it
+ */
 #ifndef STDDATA_LIST_ITERATOR_H_
 #define STDDATA_LIST_ITERATOR_H_
 
 #include <list.h>
 #include <stdbool.h>
 
+/**
+ * @brief An iterator for a @ref List
+ * 
+ * The ListIterator is mainly used for looping over a list, but might also be used to insert new
+ * items inside a list. Example usage in for loops:
+ * @code
+ * for(ListIterator iter = ListBegin(list); !ListIteratorEqual(iter, ListEnd(list)); ListIteratorIncr(&iter)) {
+ *     printf("%s\n", (char*)ListIteratorGet(iter));
+ * }
+ * @endcode
+ * 
+ * @note Most of the interface functions require only the value of an iterator to be passed, except
+ *       for the ones modifying it ( @ref ListIteratorIncr and @ref ListIteratorDecr )
+ * @note Most of the time it will be used in combination with the LISTFOR macro
+ * @warning Changing the internal variables manually outside of the interface functions will result
+ *          in an undefined behaviour.
+ * @warning Attempting to use an uninitialized ListIterator will result in an undefined behaviour
+ */
 typedef struct {
     List list;
     ListNode node;
 } ListIterator;
 
+/**
+ * @brief A macro for looping over a list
+ * 
+ * This macro creates a for loop header using iterators to iterate over a list.
+ * Example usage:
+ * @code
+ * LISTFOR(list, iter) {
+ *     printf("%s\n", (char*)ListIteratorGet(iter));
+ * }
+ * @endcode
+ * 
+ * @param list the list to be iterated over
+ * @param iter the name of the iterator that will index the list
+ * @warning @p list not being a valid allocated @ref List results in an undefined behaviour
+ */
 #define LISTFOR(list, iter)\
     for(ListIterator iter = ListBegin(list);\
         !ListIteratorEqual(iter, ListEnd(list));\
         ListIteratorIncr(&iter))
 
-/*
- * Returns an iterator to the beginning of the list
+/**
+ * @brief Returns an iterator at the beginning of the list
+ * 
+ * @param list The list to get the beginning iterator to
+ * @return An iterator pointing at the first item of the @p list
+ * @warning @p list not being a valid allocated @ref List results in an undefined behaviour
  */
 ListIterator ListBegin(List list);
 
-/*
- * Returns an iterator to the end of the list
+/**
+ * @brief Returns an iterator at the end of the list
+ * 
+ * @param list The list to get the end iterator to
+ * @return An iterator pointing at the item behind the last item of the @p list
+ * @warning @p list not being a valid allocated @ref List results in an undefined behaviour
  */
 ListIterator ListEnd(List list);
 
-/*
- * Checks if the two list iterators are equal
+/**
+ * @brief Checks if the two list iterators are equal
+ * 
+ * @return @b true if the two iterators point at the same place in the list, otherwise @b false
+ * @warning the two iterators not pointing at the same list will result in an undefined behaviour
  */
 bool ListIteratorEqual(ListIterator a, ListIterator b);
 
-/* 
- * Increments the iterator
- * the iterator cannot point to the end of the list
+/**
+ * @brief Moves the iterator to the next item in the list
+ * 
+ * @param iter the pointer to the iterator to be incremented
+ * @warning trying to increment an iterator pointing at the end of the list will result in an
+ *          undefined behaviour
  */
 void ListIteratorIncr(ListIterator *iter);
 
-/* 
- * Decrements the iterator
- * the iterator cannot point to the beginning of the list
+/**
+ * @brief Moves the iterator to the previous item in the list
+ * 
+ * @param iter the pointer to the iterator to be incremented
+ * @warning trying to increment an iterator pointing at the end of the list will result in an
+ *          undefined behaviour
  */
 void ListIteratorDecr(ListIterator *iter);
 
-/*
- * Gets an item the iterator is pointing to
+/**
+ * @brief Returns an item the iterator is pointing to
+ * 
+ * @param iter the iterator to dereference
+ * @returns the item the iterator is pointing at
  */
 void* ListIteratorGet(ListIterator iter);
 
-/*
- * Sets an item the iterator is pointing to
+/**
+ * @brief Sets an item the iterator is pointing to
+ * 
+ * @param iter the iterator for which to set the item that it is pointing at
+ * @param new_item the new value for the item
+ * @note the @p new_item may be used to hold an integer value as well if it fits inside a void*
  */
-void ListIteratorSet(ListIterator iter, void* newItem);
+void ListIteratorSet(ListIterator iter, void* new_item);
 
-/*
- * Inserts a new item after the item the iterator is pointing to
+/**
+ * @brief Inserts an item after the item the iterator is pointing to
+ * 
+ * @param iter the iterator for which to insert the item after
+ * @param new_item the value of the item to be inserted
+ * @note the @p new_item may be used to hold an integer value as well if it fits inside a void*
  */
-void ListIteratorInsertAfter(ListIterator iter, void* newItem);
+void ListIteratorInsertAfter(ListIterator iter, void* new_item);
 
-/*
- * Inserts a new item before the item the iterator is pointing to
+/**
+ * @brief Inserts an item before the item the iterator is pointing to
+ * 
+ * @param iter the iterator for which to insert the item before
+ * @param new_item the value of the item to be inserted
+ * @note the @p new_item may be used to hold an integer value as well if it fits inside a void*
  */
-void ListIteratorInsertBefore(ListIterator iter, void* newItem);
+void ListIteratorInsertBefore(ListIterator iter, void* new_item);
 
-/*
- * Removes an item iterator is pointing to and moves the iterator to the next item in the list
- * The iterator must not be pointing to the end of the list
+/**
+ * @brief Removes an item iterator is pointing to 
+ * The function removes the item the iterator is pointing to from the list 
+ * and moves the iterator to the next item in the list.
+ * 
+ * @param iter the iterator for which to remove the item
+ * @warning the iterator pointing at the end of the list results in an undefined behaviour
+ * @warning if there is another iterator pointing at the same item then trying to access that
+ *          iterator in any way later will result in an undefined behaviour
  */
 void ListIteratorRemove(ListIterator *iter);
 
--- lib/stddata/include/set.h
+++ lib/stddata/include/set.h
@@ -1,3 +1,8 @@
+/**
+ * @file set.h 
+ * @author Bartłomiej Cieślar
+ * @brief @ref Set and utilities related to it
+ */
 #ifndef STDDATA_SET_H_
 #define STDDATA_SET_H_
 
@@ -9,6 +14,13 @@
 
 extern void* SET_SENTINEL;
 
+/**
+ * @brief The structure representation of a hash set
+ * 
+ * @note Do not use raw, always use via a @ref Set type
+ * @warning Changing the internal variables manually outside of the interface functions will result
+ *          in an undefined behaviour.
+ */
 typedef struct {
     void **data;
     int alloc_power;
@@ -20,51 +32,124 @@ typedef struct {
     int max_index;
 } SetRepr;
 
+/**
+ * @brief An STL-like hash set
+ * 
+ * This is a hash set, meaning that all operations are in amortized O(1):
+ * It supports the following operations:
+ *     - @ref SetQuery Checks if an item is in the set
+ *     - @ref SetInsert Adds an item to a set
+ *     - @ref SetRemove Removes an item from a set
+ * 
+ * It can be created using @ref NewEmptySet or @ref NewFilledSet, and
+ * deallocated using @ref DeleteSet
+ * To check its size and test if it is empty, use @ref SetSize and @ref SetEmpty
+ * 
+ * @note Even though this is only a typedef, do not delcare it as a pointer to @ref SetRepr for
+ *       improved code clarity
+ * @see @ref SetIterator to loop over a set
+ */
 typedef SetRepr * Set;
 
-/*
- * Creates a new empty set
+/**
+ * @brief Creates a new empty @ref Set
+ * 
+ * Dynamically allocates a new empty @ref Set using malloc.
+ * 
+ * @param hash_func The hash function of the set
+ * @param eq_func The equality function of the set to compare the items with
+ * 
+ * @warning In order to avoid memory leaks, please use 
+ *          @ref DeleteSet to deallocate it once you are done using it.
  */
 Set NewEmptySet(unsigned long long (*hash_func)(void*), bool (*eq_func)(void*, void*));
 
-/*
- * Creates a new set filled with the given values;
+/**
+ * @brief Creates a new @ref Set with @p size items
+ * 
+ * Dynamically allocates a new empty @ref Set using malloc, and fills it with the entries in the
+ * @p contents array
+ * 
+ * @param hash_func The hash function of the set
+ * @param eq_func The equality function of the set to compare the items with
+ * @param size The size of the @p contents array
+ * @param contents The values to fill the set with
+ * @warning Passing in an array that is not an array of void* will, unlike with passing single elements,
+ *          result in an undefined behaviour.
+ * @warning @p contents having items equal to 0 or 1 will result in an undefined behaviour
+ * @warning In order to avoid memory leaks, please use 
+ *          @ref DeleteSet to deallocate it once you are done using it.
  */
 Set NewFilledSet(unsigned long long (*hash_func)(void*), bool (*eq_func)(void*, void*), int size, void* contents[]);
 
-/*
- * Deallocates a set
+/**
+ * @brief Deallocates a @ref Set
+ *
+ * @param set The set to be deallocated
+ * @warning Trying to use or deallocate already deallocated @ref Set
+ *          results in an undefined behaviour
  */
 void DeleteSet(Set set);
 
-/*
- * Returns the size of a set
+/**
+ * @brief Returns the number of items inside a set
+ * 
+ * @param set The set to check the size of
+ * @returns The size of the set
+ * @note This is not the same as the number of actually allocated entries
+ * @see @ref SetAllocSize for the number of allocated entries
+ * @warning @p set not being a valid allocated @ref Set results in an undefined behaviour
  */
 int SetSize(Set set);
 
-/*
- * Returns the allocated size of a set
+/**
+ * @brief Returns the allocated size of a set
+ * 
+ * @param set The set to check the allocated size of
+ * @returns The allocated size of the set
+ * @note This is not the same as the number of accessible entries in a set
+ * @see @ref SetSize for the number of accessible entries in a set
+ * @warning @p set not being a valid allocated @ref Set results in an undefined behaviour
  */
 int SetAllocSize(Set set);
 
-/*
- * Checks if the set is empty
+/**
+ * @brief Checks if the set is empty
+ * 
+ * @param set The set to check the emptiness of
+ * @returns @b true if the set has no items, otherwise @b false
+ * @warning @p set not being a valid allocated @ref Set results in an undefined behaviour
  */
 bool SetEmpty(Set set);
 
-/*
- * Checks if the item is in the set
+/**
+ * @brief Checks if the item is in the set
+ * 
+ * @param set The set to check the item for
+ * @param item The item to check if is in a set
+ * @returns @b true if the item is in the set, otherwise @b false
+ * @warning Passing in a pointer to 0 or 1 results in an undefined behaviour
+ * @warning @p set not being a valid allocated @ref Set results in an undefined behaviour
  */
 bool SetQuery(Set set, void* item);
 
-/*
- * Inserts a new item into a set
- * IMPORTANT Do not store any items with value of NULL or 1 (basically not legitimate pointers)
+/**
+ * @brief Inserts the item into the set
+ * 
+ * @param set The set to insert the item into
+ * @param item The item to insert into the set
+ * @warning Passing in a pointer equal to 0 or 1 results in an undefined behaviour
+ * @warning @p set not being a valid allocated @ref Set results in an undefined behaviour
  */
 void SetInsert(Set set, void* item);
 
-/*
- * Removes an item from a set
+/**
+ * @brief Removes the item from the set
+ * 
+ * @param set The set to remove the item from
+ * @param item The item to remove from the set
+ * @warning Passing in a pointer equal to 0 or 1 results in an undefined behaviour
+ * @warning @p set not being a valid allocated @ref Set results in an undefined behaviour
  */
 void SetRemove(Set set, void* item);
 
