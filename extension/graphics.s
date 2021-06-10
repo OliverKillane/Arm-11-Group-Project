fbbc5ce7cfa080041cdfc7fe8b561b3a47d70663
barci2 <bcieslar2001@gmail.com>
2021-06-10 12:13:39 +0100

    Changed directory structure and added script for generating assembly image data

--- extension/graphics.s
+++ extension/graphics.s
@@ -1,6 +1,6 @@
 @ Main file for the graphics part
-.include graphics_head.s
-.include graphics_initdraw.s
-.include graphics_blackout.s
-.include graphics_draw.s
+.include graphics/graphics_head.s
+.include graphics/graphics_initdraw.s
+.include graphics/graphics_blackout.s
+.include graphics/graphics_draw.s
 
---
+++ extension/graphics/graphics_head.s
@@ -0,0 +1,4 @@
+@ This file is only to include other files used in the main graphics routines
+.include graphics_ball.s
+.include graphics_digits.s
+.include graphics_paddle.s
