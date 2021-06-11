42daeddc2777fe222accf8ca30edcfeb9d89307b
Ioana Mihailescu <icm20@imperial.ac.uk>
2021-06-11 13:42:46 +0100

    added graphics_fill and graphics_reset functions

--- extension/graphics/graphics_head.s
+++ extension/graphics/graphics_head.s
@@ -1,6 +1,6 @@
 @ This file is only to include other files used in the main graphics routines
 .include graphics_common.s
-.include graphics_background
+.include graphics_background.s
 .include graphics_ball.s
 .include graphics_digits.s
 .include graphics_paddle.s
--- extension/graphics/graphics_reset.s
+++ extension/graphics/graphics_reset.s
@@ -1,5 +1,5 @@
 .text
-graphics_fill:
+graphics_reset:
 	
 	@ register parameters: 
 	@ r0  <- x0
@@ -14,16 +14,18 @@ graphics_fill:
 	@ r9  <- Y-axis target pixel index
 	@ r10 <- array iterator index
     @ r11 <- auxiliary register 
-	@ r12 <- write_image_buffer register
-	
+    @ r12 <- background register
+    @ r15 <- write_image_buffer register
+		
 
-	push r5
+    push r5
 	push r6
 	push r8
 	push r9
 	push r10
 	push r11
     push r12
+    push r15
 
     @ y-axis iterator
 	mov r6, r1
@@ -42,10 +44,16 @@ graphics_fill:
 	mov r11, height
 	mla r10, r0, r11, r1
 
-    mov r12 #:first8:write_image_buffer
-    orr r12 #:second8:write_image_buffer
-    orr r12 #:third8:write_image_buffer
-    orr r12 #:fourth8:write_image_buffer
+    mov r12 #:first8:background
+    orr r12 #:second8:background
+    orr r12 #:third8:background
+    orr r12 #:fourth8:background
+
+    mov r15 #:first8:write_image_buffer
+    orr r15 #:second8:write_image_buffer
+    orr r15 #:third8:write_image_buffer
+    orr r15 #:fourth8:write_image_buffer
+
 
 	condX:
 		cmp r5, r8
@@ -53,7 +61,7 @@ graphics_fill:
 
 	condY:
 		cmp r6, r9
-		ble loop @ y iterator <= target y
+		ble loop @ y-iterator <= target-y
 
 		sub r6, r6, r3
 
@@ -64,18 +72,19 @@ graphics_fill:
 		b condX	
 
 	loop:
-		ldr r11, [r10, r13]
-		str r11, [r10, r12] 
-		add r6,  r6,  #1
+        ldr r11, [r12, r10]
+		str r11, [r15, r10] 
+		add r6, r6, #1
 		add r10, r10, #1
 		b condY
 
-	end:
-		pop r12
+    end:
+        pop r15
+        pop r12
 		pop r11
 		pop r10
 		pop r9
 		pop r8
 		pop r6
 		pop r5
-		ret	
+		ret    
