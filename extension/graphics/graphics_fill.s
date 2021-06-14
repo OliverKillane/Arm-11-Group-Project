95c2880f8ec69eca53236e9da6e4a171485fcb40
Ioana Mihailescu <ioana.mihailescu20@imperial.ac.uk>
2021-06-14 22:59:48 +0100

    fixed logic and writing issues with fill

--- extension/graphics/graphics_fill.s
+++ extension/graphics/graphics_fill.s
@@ -50,12 +50,14 @@ fill:
     orr r12, r12, :second8:background
     orr r12, r12, :third8:background
     orr r12, r12, :fourth8:background
+	ldr r12, [r12]
 
 	@ load write_image_buffer values into register
     mov r4, :first8:write_image_buffer
     orr r4, r4, :second8:write_image_buffer
     orr r4, r4, :third8:write_image_buffer
     orr r4, r4, :fourth8:write_image_buffer
+	ldr r4, [r4]
 
 	@ ensure X coordinate is within image bounds
 	boundX_fill:
@@ -71,11 +73,11 @@ fill:
 
 	condY_fill:
 		cmp r6, r3
-		bgt end_fill @ Y-iterator > target-Y
+		bge end_fill @ Y-iterator > target-Y
 
 	condX_fill:
 		cmp r5, r2
-		ble loop_fill @ X-iterator <= target-X
+		blt loop_fill @ X-iterator < target-X
 
 		@ reinitialize X-iterator
 		mov r5, #0
@@ -85,21 +87,21 @@ fill:
 		b condY_fill	
 
 	loop_fill:
-		ldr r11, [r13]
+		ldr r11, [r13, #0x24]
 
 		push r2
 		push r1
 		push r0
 
 		mla r10, r6, r2, r5
-		
+		add r8, r5, r0
+		add r9, r6, r1
+
 		@ load pixel from memory to be displayed
 		ldr r0, [r11, r10, lsl #2]
 
 		@ compute array iterator
 		mov r11, width
-		add r8, r5, r0
-		add r9, r6, r1
 		mla r10, r9, r11, r8
 	
 		@ load pixel from background 
@@ -122,22 +124,33 @@ fill:
 		rsb r11, r3, #255
 
 		@ mix Red channel
+		push r7
+		and r7, r7, #0xFF
 		mul r0, r3, r0
 		mla r0, r11, r7, r0
 		add r0, r0, r0, lsl #8
-		lsl r0, #8 
+		lsl r0, #8
+		pop r7 
 
 		@ mix Green channel
+		push r7
+		mov r7, r7, lsl #8
+		and r7, r7, #0xFF
 		mul r1, r3, r1
 		mla r1, r11, r7, r1
 		add r1, r1, r1, lsl #8
 		lsl r1, #8 
+		pop r7
 
 		@ mix Blue channel
+		push r7
+		mov r7, r7, lsl #16
+		and r7, r7, #0xFF
 		mul r2, r3, r2
 		mla r2, r11, r7, r2
 		add r2, r2, r2, lsl #8
 		lsl r2, #8 
+		pop r7
 
 		@ add Green and Blue channels
 		add r0, r0, r1, lsr #8
